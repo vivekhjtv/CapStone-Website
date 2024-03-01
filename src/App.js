db9bbdca0a462ba51d3db2687c749b938eda766d
vivekhjtv <vivekhjtv18@gmail.com>
2024-03-01 12:31:40 -0500

    Change name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import rahulimage from './images/rahulimage.jpeg';
 const App = () => {
   const teamMembers = [
     {
-      name: 'Gaurav Haryani',
+      name: 'Gaurav Hariyani',
       role: 'Full-Stack Developer',
       description: `I have completed my bachelor's degree in computer applications and am currently pursuing Information Technology Solutions at Humber. I have two years of experience in full-stack domain where I have built many websites. I have expertise in AngularJS, PHP, MySQL, SQL server and deploying websites using AWS EC2 instance. My objective is to utilize my expertise to contribute to organizational projects and foster our collective advancement.`,
       githubLink: '#', // Replace with actual GitHub link
